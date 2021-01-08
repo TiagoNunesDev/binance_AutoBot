2d378cfa759f45b788af024518c3813a4ac5e98f
TiagoNunesDev <tiagonunesdev@gmail.com>
2021-01-08 23:52:48 +0000

    update

--- main.py
+++ main.py
@@ -306,6 +306,11 @@ def record_loop():
                     request_client = RequestClient(api_key=g_api_key, secret_key=g_secret_key,
                                                    url='https://testnet.binancefuture.com/')
 
+
+@app.route("/", methods=['GET'])
+def index():
+    return "<h1>Hello World!</h1>"
+
 def main():
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
