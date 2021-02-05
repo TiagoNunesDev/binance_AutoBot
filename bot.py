3a2861db77d890046f0ea9fbfe7081a3669ddb58
TiagoNunesDev <tiagonunesdev@gmail.com>
2021-02-05 21:54:11 +0000

    update

--- bot.py
+++ bot.py
@@ -298,7 +298,7 @@ class Bot:
 
                 # ---------- Set take loss -------------
                 time.sleep(2)
-                stprice = Decimal(entryPrice * 1.05)
+                stprice = Decimal(entryPrice * 1.07)
                 stprice = Decimal(stprice.quantize(Decimal(str(entryPrice)), rounding=ROUND_HALF_UP))
                 # stprice = Decimal(stprice.quantize(Decimal(str(self.minimalMove)), rounding=ROUND_HALF_UP))
 
@@ -328,7 +328,7 @@ class Bot:
 
                 # ---------- Set take loss -------------
                 time.sleep(2)
-                stprice = Decimal(entryPrice * 0.95)
+                stprice = Decimal(entryPrice * 0.93)
                 stprice = Decimal(stprice.quantize(Decimal(str(entryPrice)), rounding=ROUND_HALF_UP))
                 # stprice = Decimal(stprice.quantize(Decimal(str(self.minimalMove)), rounding=ROUND_HALF_UP))
 
